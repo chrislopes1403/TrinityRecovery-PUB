90f82c2a824c88e2c4327400d7834e6d5baf1667
Chris <chrislopes1403@comcast.net>
2021-01-14 19:12:24 -0500

    update css

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,7 +18,6 @@ use app\core\Application;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link href="//db.onlinewebfonts.com/c/08abfb8ac2e3dd415deee40206de1335?family=IcoFont" rel="stylesheet" type="text/css"/>  
     <link rel="stylesheet" type="text/css" href="/css/styles.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
 
